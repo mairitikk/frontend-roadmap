7ead29a9f425755e149e1d6ec17ff36fbd5bc4f4
mairi tikk <mairi.tikk@gmail.com>
2022-05-24 21:10:22 +0200

    calculating a value

---
+++ JavaScript/index4.js
@@ -0,0 +1,11 @@
+//performin a task
+function greet(name, lastName){
+    console.log('Hello' + name+ ''+lastName)
+}
+//Calculating a value
+function square(number) {
+    return number * number;
+}
+
+let number= square(2);
+console.log(number);
