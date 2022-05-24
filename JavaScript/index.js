1df90ed1c3c120d331c2a1cbde75f8ab3d59833b
mairi tikk <mairi.tikk@gmail.com>
2022-05-24 14:21:27 +0200

    lerning Javascript

---
+++ JavaScript/index.js
@@ -0,0 +1,13 @@
+ //comment My first JacaScript code
+ console.log('Hello Word')
+ let name='Mairi';
+ console.log(name);
+ //intrestRate
+ let intrestRate= 0.3;
+ intrestRate= 1;
+ console.log(intrestRate);
+ let name= 'Mairi';
+ let age= '30';
+ let isApproved= false;
+ let firstName= undefined;
+ let selectedColor= null;
