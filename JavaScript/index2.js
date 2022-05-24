e906a4d9de53a8c11f68e9e16e2e708b0404e683
mairi tikk <mairi.tikk@gmail.com>
2022-05-24 20:45:02 +0200

    Bracket Notation

---
+++ JavaScript/index2.js
@@ -0,0 +1,12 @@
+let person= {
+    name: 'Mairi',
+    age: '32'
+};
+
+// Dot Notation
+person.name= 'Mirju';
+
+//Bracket Notation
+let selection= 'name';
+person[selection]= 'Mary'
+console.log (person.name);
