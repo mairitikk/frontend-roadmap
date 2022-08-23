6c8a68a711e9ae05de35f28ead3327e9d836edf3
mairi tikk <mairi.tikk@gmail.com>
2022-08-23 19:10:42 +0200

    adding condicional statement

---
+++ JavaScript/condicional_statment.js
@@ -0,0 +1,10 @@
+//if today is sunny day I´ll go jogging, if not I´ll stay at home
+var isSunnyDay= false
+
+function checkIfSunny(isSunny){
+  if (isSunny === true) {
+  console.log("let`s jog!");
+  }else {
+  console.log("better stay at home");
+  }
+}
