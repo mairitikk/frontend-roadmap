4958201a32f43bf0df256d6315386cba007899ab
mairi tikk <mairi.tikk@gmail.com>
2023-04-03 20:23:53 +0200

    switch statements

--- Modern_JavaScript/js/switch_stateements.js
+++ Modern_JavaScript/js/switch_stateements.js
@@ -2,18 +2,24 @@
 
 const grade = 'D';
 
-switch(grade){}
-//using if statements
-if(grade === 'A') {
-
-} else if(grade === 'B'){
-
-} else if(grade === 'C'){
-
-} else if(grade === 'D'){
-
-} else if(grade === 'E'){
-
-} else {
-
+switch(grade){
+    case 'A':
+        console.log('you got a A!');
+        break;
+        case 'B':
+        console.log('you got a B!');
+        case 'C':
+            
+        console.log('you got a C!');
+        break;
+        case 'D':
+           
+        console.log('you got a D!');
+        break;
+        case 'E':
+        console.log('you got a E!');
+        break;
+        default:
+            console.log('not a valid grade');
 }
+//using if statements
