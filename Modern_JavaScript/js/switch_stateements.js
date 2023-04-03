9a0124cc896a64c14fa6c0a95329ae8b03025549
mairi tikk <mairi.tikk@gmail.com>
2023-04-03 20:07:36 +0200

    switch statements

---
+++ Modern_JavaScript/js/switch_stateements.js
@@ -0,0 +1,19 @@
+// switch statements
+
+const grade = 'D';
+
+switch(grade){}
+//using if statements
+if(grade === 'A') {
+
+} else if(grade === 'B'){
+
+} else if(grade === 'C'){
+
+} else if(grade === 'D'){
+
+} else if(grade === 'E'){
+
+} else {
+
+}
