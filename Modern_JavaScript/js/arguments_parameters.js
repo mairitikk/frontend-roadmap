3fd240c253299b1f6c24d36b5b6d33ea858c2578
mairi tikk <mairi.tikk@gmail.com>
2023-04-04 10:51:59 +0200

    returning values

--- Modern_JavaScript/js/arguments_parameters.js
+++ Modern_JavaScript/js/arguments_parameters.js
@@ -1,6 +1,21 @@
 // arguments and parameters
 
-const speak = function(name, time) {
-    console.log(`good day ${time} ${name}`);
-};
-speak('mario', 'morning');
+//onst speak = function(name = 'luigi', time = 'night') {
+  //  console.log(`good  ${time} ${name}`);
+//};
+//speak();
+// speak('shauns');
+
+// returning values
+ const calcArea = function(radius){
+    return 3.14 * radius**2;
+   
+ }
+
+ const area = calcArea(5);
+ console.log(area);
+
+ const calcVol = function(area){
+
+ };
+ 
