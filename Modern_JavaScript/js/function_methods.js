98b84ea30892c466015292cd41d824db0dde1e5c
mairi tikk <mairi.tikk@gmail.com>
2023-04-05 10:40:15 +0200

    function and methods

---
+++ Modern_JavaScript/js/function_methods.js
@@ -0,0 +1,10 @@
+const name = 'shaun';
+
+// functions
+const greet = () => 'hello';
+let resultOne = greet();
+console.log(resultOne);
+
+//methods
+let resultTwo = name.toUpperCase();
+console.log(resultTwo);
