0c4a82ebc37da85a11823df0a9b907ac75bc4cc9
mairi tikk <mairi.tikk@gmail.com>
2023-04-05 09:59:08 +0200

    arrow function

--- Modern_JavaScript/js/regular_arrow_functions.js
+++ Modern_JavaScript/js/regular_arrow_functions.js
@@ -9,7 +9,34 @@
 //};
 
 //simple version
-const calcArea = radius => 3.14 * radius**2;
+//const calcArea = radius => 3.14 * radius**2;
 
-const area = calcArea(5);
-console.log('area is:' , area);
+//const area = calcArea(5);
+//console.log('area is:' , area);
+
+//practise arrow function
+
+//const greet = function(){
+   // return 'hello, word';
+//};
+//const greet =() => 'hello, word';
+
+//const result = greet();
+//console.log(result);
+
+//const bill = function(products, tax){
+  //  let total = 0;
+    //for(let i = 0; i < products.length; i++){
+      //  total += products[i] +products[i] * tax;
+    //}
+    //return total;
+//}
+
+const bill =(products, tax) => {
+    let total = 0;
+    for(let i = 0; i < products.length; i++){
+        total += products[i] +products[i] * tax;
+    }
+    return total;
+}
+console.log(bill([10, 15, 30], 0.2))
