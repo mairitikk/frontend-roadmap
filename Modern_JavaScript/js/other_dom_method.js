e99f8eee137902949cd711709cdf8e8138cb68b0
mairi tikk <mairi.tikk@gmail.com>
2023-04-09 19:53:36 +0200

    other dom method

---
+++ Modern_JavaScript/js/other_dom_method.js
@@ -0,0 +1,11 @@
+//get an element by ID
+const title = document.getElementById('page-title');
+console.log(title);
+// get elements by their class name
+const errors = document.getElementsByClassName('error');
+console.log(errors);
+
+// get element by their tag name
+const paras = document.getElementsByTagName('p');
+console.log(paras);
+console.log(paras[2]);
