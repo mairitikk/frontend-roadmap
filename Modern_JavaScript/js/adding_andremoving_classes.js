2983c382599a7b43f53f60bff78f474ab8eb9149
mairi tikk <mairi.tikk@gmail.com>
2023-04-11 10:01:55 +0200

    adding and removing classes

---
+++ Modern_JavaScript/js/adding_andremoving_classes.js
@@ -0,0 +1,19 @@
+//const content = document.querySelector('p');
+// get classes that element has
+//console.log(content.classList);
+
+
+// how to add classes
+//content.classList.add('error');
+
+//how to remove class
+//content.classList.remove('error');
+//content.classList.add('success');
+
+const paras = document.querySelectorAll('p');
+
+paras.forEach(p => {
+    if(p.textContent.includes('error')){
+        p.classList.add('error');
+    }
+});
