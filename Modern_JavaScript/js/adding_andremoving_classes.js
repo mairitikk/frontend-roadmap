f37e5a4e681742fd9302a7f50e9ca003ddfa9bcf
mairi tikk <mairi.tikk@gmail.com>
2023-04-11 10:33:39 +0200

    adding class if includes textContent

--- Modern_JavaScript/js/adding_andremoving_classes.js
+++ Modern_JavaScript/js/adding_andremoving_classes.js
@@ -16,4 +16,7 @@ paras.forEach(p => {
     if(p.textContent.includes('error')){
         p.classList.add('error');
     }
+    if(p.textContent.includes('success')){
+        p.classList.add('success');
+    }
 });
