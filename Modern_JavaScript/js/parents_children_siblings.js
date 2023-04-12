1e716b9ad7b268afc1fbb6f2c2b7477fab55e85d
mairi tikk <mairi.tikk@gmail.com>
2023-04-12 13:17:17 +0200

    finding parent element

---
+++ Modern_JavaScript/js/parents_children_siblings.js
@@ -0,0 +1,20 @@
+const article = document.querySelector('article');
+
+//console.log(article.children);
+//console.log(Array.from(article.children));
+//console.log(article.children);
+
+// lisad lapse elemendile classi
+
+Array.from(article.children).forEach(child => {
+    child.classList.add('article-element');
+});
+
+//finding parent element
+
+const title = document.querySelector('h2');
+
+console.log(title.parentElement);
+console.log(title.parentElement.parentElement);
+
+//finding sibling element
