aec2c57793fc782e342e1252507c5486b18df4d6
mairi tikk <mairi.tikk@gmail.com>
2023-04-13 12:05:44 +0200

    adding li element top and end of de list

--- Modern_JavaScript/js/click_event.js
+++ Modern_JavaScript/js/click_event.js
@@ -3,7 +3,14 @@ const ul = document.querySelector('ul');
 
 button.addEventListener('click', () => {
     //console.log('you cliked me');
-    ul.innerHTML += '<li>something new</li>';
+   // ul.innerHTML += '<li>something new</li>';
+   // teine moodus
+   const li = document.createElement('li');
+   li.textContent = 'someting new to do';
+   // lisab listi l6ppu
+   //ul.append(li);
+   // kui ylesse taseme lisada
+   ul.prepend(li);
 });
 
 const items = document.querySelectorAll('li');
