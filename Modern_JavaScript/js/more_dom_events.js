36784527ff3950e6119a5038c6cde712eb89a90c
mairi tikk <mairi.tikk@gmail.com>
2023-04-15 21:36:08 +0200

    mouse moving cordinats

--- Modern_JavaScript/js/more_dom_events.js
+++ Modern_JavaScript/js/more_dom_events.js
@@ -1,5 +1,13 @@
+// et teksti kopeeria ei saaks
 const copy =document.querySelector('.copy-me');
 
 copy.addEventListener('copy', () =>{
     console.log('OI! my content is copyright');
+});
+
+//annab kordinaadid kui hiir boxi sees liigub
+const box = document.querySelector('.box');
+box.addEventListener('mousemove', e =>{
+    //console.log(e);
+    console.log(e.offsetX, e.offsetY);
 });
