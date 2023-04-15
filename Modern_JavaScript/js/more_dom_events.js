d61247fe45b658167fbba3cc29c284f136bf44a2
mairi tikk <mairi.tikk@gmail.com>
2023-04-15 21:49:30 +0200

    mouse wheel cordinates

--- Modern_JavaScript/js/more_dom_events.js
+++ Modern_JavaScript/js/more_dom_events.js
@@ -9,5 +9,12 @@ copy.addEventListener('copy', () =>{
 const box = document.querySelector('.box');
 box.addEventListener('mousemove', e =>{
     //console.log(e);
-    console.log(e.offsetX, e.offsetY);
+    //console.log(e.offsetX, e.offsetY);
+
+    // kuvab kordinaadid boxi peale
+    box.textContent = `x pos - ${e.offsetX}   y pos -${e.offsetY}`;
+});
+// kui lehekylge kerida annab kordinaadid kus hiir on
+document.addEventListener('wheel', e => {
+    console.log(e.pageX, e.pageY);
 });
