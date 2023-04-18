0d206d3412ac83d790e6a1ed015231734889bc67
mairi tikk <mairi.tikk@gmail.com>
2023-04-18 09:52:34 +0200

    testing RegEx paterns

--- Modern_JavaScript/js/submit_events.js
+++ Modern_JavaScript/js/submit_events.js
@@ -1,8 +1,15 @@
 const form = document.querySelector('.ssignup-form');
-const username = document.querySelector('#username');
+//const username = document.querySelector('#username');
 
 
-form.addEventListener('submit' , e => {
-    e.preventDefault();
-    console.log(username.value);
-});
+//form.addEventListener('submit' , e => {
+    //e.preventDefault();
+    //console.log(username.value);
+   // console.log(form.username.value)
+//});
+
+//testing RegEx 
+const username = 'shaunas';
+const pattern = /^[a-z]{6,}$/;
+let result = pattern.test(username);
+console.log(result);
