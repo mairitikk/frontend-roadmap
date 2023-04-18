c16be357263f65e0281a78cd00a18b89267bd1e9
mairi tikk <mairi.tikk@gmail.com>
2023-04-18 09:56:49 +0200

    regEx test

--- Modern_JavaScript/js/submit_events.js
+++ Modern_JavaScript/js/submit_events.js
@@ -9,7 +9,13 @@ const form = document.querySelector('.ssignup-form');
 //});
 
 //testing RegEx 
-const username = 'shaunas';
+const username = 'sha';
 const pattern = /^[a-z]{6,}$/;
 let result = pattern.test(username);
-console.log(result);
+
+if(result){
+    console.log('regex test passed');
+}
+else {
+    console.log('regex test failed');
+}
