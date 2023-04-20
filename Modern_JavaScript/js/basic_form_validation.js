29d2deaf8dd1698f68b9dc22e664d26fcb94edff
mairi tikk <mairi.tikk@gmail.com>
2023-04-20 21:22:59 +0200

    live feedback

--- Modern_JavaScript/js/basic_form_validation.js
+++ Modern_JavaScript/js/basic_form_validation.js
@@ -1,12 +1,13 @@
 const form = document.querySelector('.signup-form');
 const feedback = document.querySelector('.feedback');
+const usernamePattern = /^[a-zA-Z]{6,12}$/;
 
 form.addEventListener('submit' , e => {
     e.preventDefault();
 
     //validation
     const username  = form.username.value;
-    const usernamePattern = /^[a-zA-Z]{6,12}$/;
+    
 
     if(usernamePattern.test(username)){
         //feedback good info
@@ -16,4 +17,14 @@ form.addEventListener('submit' , e => {
         //feedback help info
         feedback.textContent = 'username must contain letters only & be between 6 & 12 characters long ';
     }
+});
+//live feedback
+form.username.addEventListener('keyup', e => {
+   // console.log(e.target.value, form.username.value);
+   if(usernamePattern.test(e.target.value)){
+    form.username.setAttribute('class', 'success');
+   }
+   else {
+    form.username.setAttribute('class', 'error');
+   }
 });
