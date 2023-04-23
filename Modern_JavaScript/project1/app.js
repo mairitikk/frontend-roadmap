847afeb54a268c0f7f0638eb6228483565456d64
mairi tikk <mairi.tikk@gmail.com>
2023-04-23 20:28:42 +0200

    adding value

--- Modern_JavaScript/project1/app.js
+++ Modern_JavaScript/project1/app.js
@@ -1,4 +1,11 @@
 const correctAnswers = ['B', 'B', 'B', 'B'];
 const form = document.querySelector('.quiz-form');
 
-form.addEventListener('submit', e =>)
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    let score = 0;
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value]
+
+    // check answers
+});
